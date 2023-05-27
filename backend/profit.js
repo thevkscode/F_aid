2c465ce6a403b2f9acc53c310d5861951f96ce44
thevkscode <singhvivek02099@gmail.com>
2023-05-28 05:05:52 +0530

    updated backend

--- backend/profit.js
+++ backend/profit.js
@@ -1,51 +1,48 @@
-module.exports = function (rsi1, rsi2, closingPrice1, closingPrice2, si, ei){
-    let arr=[];
-    for(let j=si;j<=ei;j++)
-    {
-        arr.push(rsi1[j]-rsi2[j]);
-
+module.exports = function (rsi1, rsi2, closingPrice1, closingPrice2, si, ei) {
+  let arr = [];
+  //relative gap
+  for (let j = si; j <= ei; j++) {
+    arr.push(rsi1[j] - rsi2[j]);
+  }
+  let indices = [];
+  let i = si;
+  indices.push(si);
+  //find trading point
+  while (i < ei) {
+    let a = false,
+      b = false;
+    while (i < ei && arr[i] >= arr[i + 1]) {
+      a = true;
+      i++;
     }
-    let indices=[];
-    let i=si;
-    indices.push(si);
-    while(i<ei)
-    {
-        let a=false,b=false;
-        while(i<ei&&arr[i]>=arr[i+1])
-        {
-            a=true;
-            i++;
-        }
-        if(a)
-        {
-            indices.push(i);
-        }
-        while(i<ei&&arr[i]<=arr[i+1])
-        {
-            b=true;
-            i++;
-        }
-        if(b)
-        {
-            indices.push(i);
-        }
-        i++;
+    if (a) {
+      indices.push(i);
     }
-    if(indices.length<=1)
-    return 0;
-    let profit=0;
-    let n=indices.length;
-    for(let j=1;j<n;j++)
-    {
-        let sum=(closingPrice1[j]-closingPrice1[j-1])+(closingPrice2[j-1]-closingPrice2[j]);
-        if(sum<0)
-        {
-            sum=0-sum;
-        }
-        profit+=sum;
+    while (i < ei && arr[i] <= arr[i + 1]) {
+      b = true;
+      i++;
     }
-    return {
-        value :profit,
-        period: indices
-    };
-}
+    if (b) {
+      indices.push(i);
+    }
+    i++;
+  }
+  if (indices.length <= 1) return 0;
+  let profit = 0;
+  let n = indices.length;
+  //profit calculation by taking long position on underpefroming(buy-sell) and short position on outperforming(sell-buy)
+  for (let j = 1; j < n; j++) {
+    let sum =
+      closingPrice1[indices[j]] -
+      closingPrice1[indices[j - 1]] +
+      (closingPrice2[indices[j - 1]] - closingPrice2[indices[j]]);
+    if (sum < 0) {
+      sum = 0 - sum;
+    }
+    profit += sum;
+  }
+  return {
+    value: profit,
+    period: indices,
+  };
+};
